a588363f082ca005fcc8456bd58db1fe0d9544f8
khurram <ks2066420@gmail.com>
2024-01-13 20:59:12 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a Term project which is related to Diabetes Prediction, a helpful project to examine either the patient is positice or negative. 
+adding some more information:
+1-loaded tha dataset
+2-analyze the features
+3-also added the important aspects, such as info, description and other....
+4- Did the Univariate, bivariate and Multivariate 
+5- Did analysis for each feature, and try to add the graphs for more comprehensive information
+6-Split the Dataset for more, training and testing( now, apply allgorithms,and then Prediction)
+-----------------------------------------------------------------------------------------------------
+I faced error, while Keras and tensorflow installation, and that is also due to wrong version of python
+we faced it earlier and now again, so before working also check out the compatiability of library with python./
+if you want to work with it, then use the low versions such as 3.9, 3.10 and 3.11, as most latest makes the error.
+
---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+from flask import Flask, render_template, request
+
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__, template_folder='template')
+
+# Update the model path to the correct one
+model_path = 'C:/Users/Bravo_is_Back/Downloads/Diabetes-Prediction-System-main/saved_models/diabetes_model.h5'
+model = load_model(model_path)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        pregnancies = float(request.form['pregnancies'])
+        glucose = float(request.form['glucose'])
+        blood_pressure = float(request.form['blood_pressure'])
+        skin_thickness = float(request.form['skin_thickness'])
+        insulin = float(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        dpf = float(request.form['diabetes_pedigree_function'])
+        age = float(request.form['age'])
+
+        new_data = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]]
+        prediction = model.predict(np.array(new_data))
+        outcome = "diabetic-1" if prediction > 0.5 else "not diabetic-0"
+
+        return render_template('result.html', outcome=outcome)
+
+if __name__ == '__main__':
+    app.run(debug=True)
